064ef9449058b9c69df1948902103402e442e78b
Ariel Lindemann <ga92xan@mytum.de>
2021-12-03 15:08:24 +0100

    day 3 part 2

--- day03/03.hs
+++ day03/03.hs
@@ -2,16 +2,22 @@ import System.Environment (getArgs)
 findBinary :: [[Int]] -> [Int]
 findBinary l@(x:xs)
   | null x = []
-  | otherwise = mostCommon (firsts l) : findBinary (map tail l)
+  | otherwise = mostCommon (nth l 0) : findBinary (map tail l)
 findBinary _ = []
 
-firsts :: [[a]] -> [a]
-firsts ((x:xs):ys) = x:firsts ys
-firsts _ = []
+nth :: [[a]] -> Int -> [a]
+nth l i= [k!!i | k <- l]
+
+flipBit :: Int -> Int
+flipBit x = (x + 1) `mod` 2
 
 mostCommon :: [Int] -> Int
-mostCommon l@(x:xs) = if length (filter (== x) l) > length l `div` 2 then x else ((x+1) `mod` 2)
-mostCommon _ = 0
+mostCommon l@(x:_)
+  | r == 0 = 1
+  | r < 0 = x
+  | otherwise = flipBit x
+  where r = length (filter (/=x) l) - length (filter (==x) l)
+mostCommon _  = 0
 
 toDecimal :: [Int] -> Int
 toDecimal [] = 0
@@ -21,12 +27,28 @@ parseBin :: String -> [Int]
 parseBin (x:xs) = read [x] : parseBin xs
 parseBin _ = []
 
+filterCriteria :: [[Int]] -> Int -> Bool -> [Int]
+filterCriteria l i com
+  | length l <= 1 = head l
+  | otherwise = filterCriteria (filter (f i com) l) (i + 1) com
+  where f i com l'
+          | com = l'!!i == mostCommon (nth l i)
+          | otherwise = l'!!i /= mostCommon (nth l i)
+
 main :: IO ()
 main = do
   args <- getArgs
   contents <- readFile $ head args
 
   let nums = map parseBin $ lines contents
+
+  --part 1
   let gamma = findBinary nums
-  let epsilon = map (\x -> (x + 1) `mod` 2) gamma
+  let epsilon = map flipBit gamma
   print $ toDecimal gamma * toDecimal epsilon
+
+  --part 2
+  let oxygen = toDecimal $ filterCriteria nums 0 True
+  let co2 = toDecimal $ filterCriteria nums 0 False
+
+  print $ oxygen * co2
