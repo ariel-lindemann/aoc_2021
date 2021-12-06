3324e5cbb9c6a2e7b7ff7af4822a249d5373cb8e
Ariel Lindemann <ga92xan@mytum.de>
2021-12-06 02:30:18 +0100

    day 5 part 2

--- day05/05.hs
+++ day05/05.hs
@@ -1,26 +1,24 @@
 import System.Environment (getArgs)
-import Data.Set (fromList)
 import Data.List (group, sort)
+
 type Point = (Int, Int)
 type Line = (Point, Point)
 type Grid = [[Int]]
 
-pointsInLine :: Line -> [Point]
-pointsInLine ((x1, y1), (x2, y2))
+pointsInLine :: Bool -> Line -> [Point]
+pointsInLine diag ((x1, y1), (x2, y2))
   | x1 == x2 = [(x1, y') | y' <- inters y1 y2]
   | y1 == y2 = [(x', y1) | x' <- inters x1 x2]
-  | otherwise = []
+  | otherwise = if diag then dPoints else []
   where inters a b
           | a > b = [b..a]
           | otherwise = [a..b]
+        dPoints = [(x1 + sX * d, y1 + sY * d) | d <- [0..(abs (x1-x2))]]
+        sX = signum (x2-x1)
+        sY = signum (y2-y1)
 
 pointsOccurence :: Int -> [Point] -> Int
 pointsOccurence n ps = length $ (map head . filter (\x -> length x >= n) . group . sort) ps
-
-intersectionGrid :: [Point] -> Grid -> Grid
-intersectionGrid ((x,y):ps) g = intersectionGrid ps g'
-  where g' = undefined
-intersectionGrid [] g = g
 
 parseLine :: String -> Line
 parseLine s = (parsePoint $ head p, parsePoint $ last p)
@@ -41,6 +39,10 @@ main = do
   args <- getArgs
   contents <- readFile $ head args
 
-  let points = concatMap (pointsInLine . parseLine) (lines contents)
-  print 2
+  --part1
+  let points = concatMap (pointsInLine False . parseLine) (lines contents)
   print $ pointsOccurence 2 points
+
+  --part2
+  let diagPoints = concatMap (pointsInLine True . parseLine) (lines contents)
+  print $ pointsOccurence 2 diagPoints
